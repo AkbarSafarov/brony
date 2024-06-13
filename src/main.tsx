bae27fe0110d048434edcac81bf7d2ae87cec96c
Akbar <akbarsafarov.as@gmail.com>
2024-06-13 23:02:32 +0500

    main-page

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import { createRoot } from "react-dom/client";
+import {BrowserRouter} from "react-router-dom";
 import App from './App.tsx'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+const root = document.getElementById('root');
+
+if (!root) {
+    throw new Error('root not found!');
+}
+
+const container = createRoot(root);
+
+container.render(
+    <BrowserRouter>
+        <App/>
+    </BrowserRouter>
 )
---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Header } from "../../components";
+
+export const Home: React.FC = () => {
+    return (
+        <>
+            <Header />
+
+
+        </>
+
+    );
+};
